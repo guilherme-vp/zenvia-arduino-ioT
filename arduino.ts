d065d3da07bcbc7af3825274566cde8ce61dcc03
Xubili <guivpw68@gmail.com>
2020-10-16 00:14:19 -0300

    added arduino j5 code & socket configs

--- arduino.ts
+++ arduino.ts
@@ -1,60 +1,68 @@
-import io from 'socket.io-client'
 import five from 'johnny-five'
-import config from './configurations'
-
-const port: string | undefined = `${config.port}`
-
-const socket = io.connect(`https://zenvia-server.herokuapp.com/`)
+// import { socket } from './src/config/socket'
+import { WhatsMessage } from './src/types/message'
+import io from 'socket.io-client'
 
 const board = new five.Board({ port: 'COM4' })
 
+const socket = io('http://localhost:3000')
+
 board.on('ready', () => {
-	const led = new five.Led({ pin: 2 })
+	const led = new five.Led({ pin: 13 })
 	const lcd = new five.LCD({
 		controller: 'PCF8574T'
 	})
-	socket.on('connect', () => {
-		lcd.clear()
-		led.blink()
 
-		lcd.cursor(0, 3).print('Bem vindo!')
-		setTimeout(() => {
-			led.stop(0)
-			led.off()
-			lcd
-				.clear()
-				.cursor(0, 3)
-				.print('Aproveite')
-				.cursor(1, 2)
-				.print('os exemplos')
-		}, 2000)
-		setTimeout(() => {
-			lcd
-				.clear()
-				.cursor(0, 0)
-				.print('de uso das APIs')
-				.cursor(1, 3)
-				.print('da Zenvia!')
-		}, 4000)
-		setTimeout(() => {
-			lcd
-				.clear()
-				.cursor(0, 3)
-				.print('configure')
-				.cursor(1, 1)
-				.print('os seus dados')
-		}, 6000)
-		setTimeout(() => {
-			lcd.clear().cursor(0, 2).print('e aproveite!')
-		}, 9000)
-		setTimeout(() => {
-			lcd.clear()
-			lcd.noBacklight()
-		}, 11000)
-	})
+	if (socket.disconnected)
+		lcd.cursor(0, 1).print('Nao conectado').cursor(1, 3).print('ao socket')
+	else {
+		socket.on('connect', () => {
+			led.blink()
 
-	socket.on('newMessage', (data: string) => {
-		console.log('data arduino', data)
-		lcd.backlight().print('hellow')
-	})
+			lcd.clear().cursor(0, 3).print('Bem vindo!')
+
+			lcd.useChar('bigpointerright')
+			setTimeout(() => {
+				led.stop(0)
+				led.off()
+				lcd
+					.clear()
+					.cursor(0, 3)
+					.print('Aproveite')
+					.cursor(1, 2)
+					.print('os exemplos')
+			}, 2000)
+			setTimeout(() => {
+				lcd
+					.clear()
+					.cursor(0, 0)
+					.print('de uso das APIs')
+					.cursor(1, 2)
+					.print('da Zenvia!:bigpointerright::bigpointerright:')
+			}, 4000)
+			setTimeout(() => {
+				lcd
+					.clear()
+					.cursor(0, 3)
+					.print('configure')
+					.cursor(1, 1)
+					.print('os seus dados')
+			}, 6000)
+			setTimeout(() => {
+				lcd.clear().cursor(0, 1).print('e divirta-se!')
+			}, 9000)
+			setTimeout(() => {
+				lcd.clear()
+				lcd.noBacklight()
+			}, 11000)
+			socket.on('receivedMessage', (msg: WhatsMessage) => {
+				const { receiver, message } = msg
+				console.log('data arduino', msg)
+				lcd.backlight().print(receiver).cursor(1, 0).print('recebeu')
+				setTimeout(() => {
+					lcd.clear().print(message)
+				}, 2000)
+			})
+		})
+	}
 })
